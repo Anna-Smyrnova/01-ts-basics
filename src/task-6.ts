7330e18947228ac7a9ebf2057bac9a73a4f16992
Anna Smyrnova <anyto4ka777s@gmail.com>
2026-03-15 19:12:19 +0200

    fixed task 6 and 8 files

--- src/task-6.ts
+++ src/task-6.ts
@@ -6,5 +6,5 @@ getFirstElement<number> ([1, 2, 3]);           // 1
 getFirstElement<string> (["a", "b", "c"]);     // "a"
 getFirstElement<boolean> ([true, false, true]); // true
 
-const firstEl = getFirstElement<number>([1, "a", true]);
+
 
